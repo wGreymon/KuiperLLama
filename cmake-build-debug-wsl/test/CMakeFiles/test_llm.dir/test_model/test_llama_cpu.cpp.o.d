88618f67dc4c32ca83a1bc4469fde42de421b070
wGreymon <3555821172@qq.com>
2024-12-26 11:32:54 +0800

    Remove cmake-build directories from version control

--- cmake-build-debug-wsl/test/CMakeFiles/test_llm.dir/test_model/test_llama_cpu.cpp.o.d
+++
@@ -1,3 +0,0 @@
-test/CMakeFiles/test_llm.dir/test_model/test_llama_cpu.cpp.o: \
- /home/wgreymon/kuiper/KuiperLLama/test/test_model/test_llama_cpu.cpp \
- /usr/include/stdc-predef.h
--- cmake-build-debug-wsl/test/CMakeFiles/test_llm.dir/test_op/test_cu_rope.cpp.o.d
+++
@@ -1,3 +0,0 @@
-test/CMakeFiles/test_llm.dir/test_op/test_cu_rope.cpp.o: \
- /home/wgreymon/kuiper/KuiperLLama/test/test_op/test_cu_rope.cpp \
- /usr/include/stdc-predef.h
